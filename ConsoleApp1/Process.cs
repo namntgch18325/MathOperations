3993095ff797393206115101198e3ae4b6b34e69
namntgch18325 <namntgch18325@fpt.edu.vn>
2020-09-24 17:24:21 +0700

    [MathOperations] Push bai tap

---
+++ ConsoleApp1/Process.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class Process
+    {
+        public static void Run()
+        {
+            MathOperations math = new MathOperations();
+            Console.WriteLine("Int " + math.Add(1, 1));
+            Console.WriteLine("Double " + math.Add(1.5f, 1.5f, 1.5f));
+            Console.WriteLine("Decimal " + math.Add(1m, 2m, 3m));
+        }
+    }
+}
